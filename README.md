27061f77d707fbca8b86c71899ef302aeb86682c
Sina <sinamindev@gmail.com>
2017-09-13 14:16:04 -0700

    ReadMe+ documentation

--- README.md
+++ README.md
@@ -37,6 +37,22 @@ $ git push heroku master
 $ heroku run python manage.py migrate
 $ heroku open
 ```
-## Documentation
+## Documentation for assinging Peers to Review videos
+The peer assignment algorithm runs at O(NM) time complexity, which is Quadratic.
+
+This is because the for-loop runs N times and contains a call to random.sample(), which has an O(M) time complexity.
+
+Documentation for [random.sample()](https://hg.python.org/cpython/file/ab500b297900/Lib/random.py#l267)
 
 ## Design Choices
+We use a list to store review assignments for each peer
+
+We Use a deque to randomly sample M number of reviews to assign to each peer
+ 
+A deque is used so we can pop out the value corresponding to the peer being assigned before randomly choosing from the remaining values in the deque in order to prevent two things:
+ 1. Prevents assigning a peer to themselves
+ 2. Prevents assigning duplicate reviews to the same peer
+
+At the end of every loop, the peer value that was popped out of the deque is appended to the back
+
+The list of assigned reviews is returned after every peer has been randomly assigned reviews
--- peerReview/peer_review.py
+++ peerReview/peer_review.py
@@ -6,11 +6,11 @@ from collections import deque
 # IN: N == number of peers
 # IN: M == number of reviews
 # OUT: assignments == list of assigned reviews for each peer
-# logic:	We use a list to store lists of assigned reviews for each peer
+# logic:	We use a list to store review assignments for each peer
 # 			We Use a deque to randomly sample M number of reviews to assign to each peer
 #			A deque is used so we can pop out the value corresponding to the peer being assigned,
-#	 		 before randomly choosing the remaining values from the deque in order to prevent two things
-#			 1. Prevents ssigning a peer to themselves
+#	 		 before randomly choosing from the remaining values in the deque in order to prevent two things
+#			 1. Prevents assigning a peer to themselves
 #			 2. Prevents assigning duplicate reviews to the same peer
 #			At the end of every loop, the peer value that was popped out of the deque is appended to the back
 #			The list of assigned reviews is returned after every peer has been randomly assigned reviews
@@ -33,7 +33,7 @@ def getPeerAssignments(N, M):
 # FOR-LOOP
 # deque([2, 3, 4, 5])										# RANGE.popleft()
 # [[3, 5, 2]]												# assignments.append(sample(RANGE, M))
-# deque([2, 3, 4, 5, 1])									# RANGE.append(i)
+# deque([2, 3, 4, 5, 1])									# RANGE.appen d(i)
 # deque([3, 4, 5, 1])										# RANGE.popleft()
 # [[3, 5, 2], [3, 5, 4]]									# assignments.append(sample(RANGE, M))
 # deque([3, 4, 5, 1, 2])									# RANGE.append(i)
